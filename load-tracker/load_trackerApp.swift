bb84cbefdae1f6787f1fac1717474e1568226b27
Kosuke Murakami <kosuke.murakami@muilab.com>
2026-03-20 18:33:35 +0900

    Initial Commit

---
+++ load-tracker/load_trackerApp.swift
@@ -0,0 +1,32 @@
+//
+//  load_trackerApp.swift
+//  load-tracker
+//
+//  Created by 村上航輔 on 2026/03/20.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct load_trackerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ load-trackerTests/load_trackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  load_trackerTests.swift
+//  load-trackerTests
+//
+//  Created by 村上航輔 on 2026/03/20.
+//
+
+import Testing
+@testable import load_tracker
+
+struct load_trackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
